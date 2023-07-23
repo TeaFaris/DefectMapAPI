0cc1bab07adba83e13a42376df7413f159bb50a2
TeaFaris <teafaris@organizatsiya.site>
2023-07-24 01:50:20 +0500

    Added UploadedFiles table to DBContext Changed all properties in UploadedFile from set to init

--- DefectMapAPI/Data/ApplicatonDbContext.cs
+++ DefectMapAPI/Data/ApplicatonDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DefectMapAPI.Models.File;
+using Microsoft.EntityFrameworkCore;
 
 namespace DefectMapAPI.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<UploadedFile> UploadedFiles { get; init; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) { }
     }
