d4b88285c684ad278d5a0dd184020e608bcf0ca6
TeaFaris <teafaris@organizatsiya.site>
2023-07-24 01:43:32 +0500

    File model added

---
+++ DefectMapAPI/Models/File/UploadedFile.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DefectMapAPI.Models.File
+{
+    public class UploadedFile
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required]
+        public string FileName { get; set; }
+        [Required]
+        public string StoredFileName { get; set; }
+        [Required]
+        public string ContentType { get; set; }
+    }
+}
