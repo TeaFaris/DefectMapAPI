3031c7b38d596fa21c54032b3082ef19fcfb4516
TeaFaris <teafaris@organizatsiya.site>
2023-07-24 04:51:17 +0500

    User model added

---
+++ DefectMapAPI/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DefectMapAPI.Models
+{
+    public class ApplicationUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+    }
+}
