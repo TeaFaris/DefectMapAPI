60791c34477a7fc86cee8a5ee2bb2a47b6a80cf2
TeaFaris <teafaris@organizatsiya.site>
2023-07-25 05:48:41 +0500

    Created defect model

---
+++ DefectMapAPI/Models/Defect.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DefectMapAPI.Models
+{
+    public class Defect
+    {
+        [Key]
+        public int Id { get; init; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public List<Guid> PhotosIds { get; set; }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double HorizontalAccuracy { get; set; }
+        public double VerticalAccuracy { get; set; }
+    }
+}
