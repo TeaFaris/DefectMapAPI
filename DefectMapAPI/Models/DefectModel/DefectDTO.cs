5c1cbb95918c3f75605cbdc6ab396ae997b8d8ac
TeaFaris <teafaris@organizatsiya.site>
2023-07-25 06:39:42 +0500

    Added DefectDTO and Defect mapper

---
+++ DefectMapAPI/Models/DefectModel/DefectDTO.cs
@@ -0,0 +1,30 @@
+﻿using DefectMapAPI.Models.UserModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using DefectMapAPI.Models.File;
+
+namespace DefectMapAPI.Models.DefectModel
+{
+    public class DefectDTO
+    {
+        [Key]
+        public int Id { get; init; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public List<UploadedFile> Photos { get; set; }
+
+        [Required]
+        public UserDTO Owner { get; init; }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double HorizontalAccuracy { get; set; }
+        public double VerticalAccuracy { get; set; }
+    }
+}
---
+++ DefectMapAPI/Models/DefectModel/DefectMapper.cs
@@ -0,0 +1,32 @@
+﻿using DefectMapAPI.Models.File;
+using DefectMapAPI.Models.UserModel;
+using DefectMapAPI.Services.Repositories.File;
+
+namespace DefectMapAPI.Models.DefectModel
+{
+    public static class DefectMapper
+    {
+        public static async Task<DefectDTO> ToDTO(this Defect.Defect defect, IFileRepository fileRepository)
+        {
+            var photos = new List<UploadedFile>(defect.PhotosIds.Count);
+
+            foreach(var photoId in defect.PhotosIds)
+            {
+                photos.Add((await fileRepository.GetAsync(photoId))!);
+            }
+
+            return new DefectDTO
+            {
+                Id = defect.Id,
+                Description = defect.Description,
+                HorizontalAccuracy = defect.HorizontalAccuracy,
+                Latitude = defect.Latitude,
+                Longitude = defect.Longitude,
+                Name = defect.Name,
+                Owner = defect.Owner.ToDTO(),
+                VerticalAccuracy = defect.VerticalAccuracy,
+                Photos = photos
+            };
+        }
+    }
+}
