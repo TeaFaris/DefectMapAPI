a14a1a8a7b26d89449f0c4793cb510c4ded58f96
TeaFaris <teafaris@organizatsiya.site>
2023-07-25 06:33:08 +0500

    Created UserDTO and UserMapper

---
+++ DefectMapAPI/Models/UserModel/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DefectMapAPI.Models.UserModel
+{
+    public class UserDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Role { get; set; }
+    }
+}
